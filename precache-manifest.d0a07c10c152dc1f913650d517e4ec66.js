5ab4786e81b8ed59b3db5a9a61c01cfa698c541f
bob <yxq477@gamil.com>
2020-09-18 16:32:32 +0800

    Updates

---
+++ precache-manifest.d0a07c10c152dc1f913650d517e4ec66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df9f5c1afe7b4ccfe3fc468515b04aec",
+    "url": "/ts-seek-truth/index.html"
+  },
+  {
+    "revision": "e43a1b8a810f2365eff2",
+    "url": "/ts-seek-truth/static/css/main.9254466e.chunk.css"
+  },
+  {
+    "revision": "f03d11ce1e7475385434",
+    "url": "/ts-seek-truth/static/js/2.3fcd3b84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ts-seek-truth/static/js/2.3fcd3b84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e43a1b8a810f2365eff2",
+    "url": "/ts-seek-truth/static/js/main.6ff739e3.chunk.js"
+  },
+  {
+    "revision": "f1c2df9cb502bec78ef8",
+    "url": "/ts-seek-truth/static/js/runtime-main.039e178b.js"
+  }
+]);
